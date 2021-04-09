b4849f1f1c16e2810fe7166c80ab6a9f6c5720c7
mukilanik <ak-muki@hotmail.com>
2021-04-09 12:16:17 +0530

    Refactor

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,21 +1,22 @@
-import { useRouter } from 'next/router';
-import React, { useContext, useEffect } from 'react'
-import {WebSocketContext} from '../components/providers/WebSocketProvider';
-import useSaveQueryParamsToken from '../components/stores/useSaveQueryParamsToken'
+import { useRouter } from "next/router";
+import React, { useContext, useEffect } from "react";
+import { WebSocketContext } from "../components/providers/WebSocketProvider";
+import useSaveQueryParamsToken from "../components/stores/useSaveQueryParamsToken";
 
 const Login: React.FC = () => {
-    useSaveQueryParamsToken();
-    const { socketStatus } = useContext(WebSocketContext)
-    const router = useRouter();
-    useEffect(() => {
-        if(socketStatus==="connected")
-            router.push("/home");
-    },[socketStatus])
-    return (
-        <div>
-            <a href={`${process.env.NEXT_PUBLIC_SERVER}/api/auth/oauth`}>Sign in with google</a>
-        </div>
-    )
-}
+  useSaveQueryParamsToken();
+  const { socketStatus } = useContext(WebSocketContext);
+  const router = useRouter();
+  useEffect(() => {
+    if (socketStatus === "connected") router.push("/home");
+  }, [socketStatus]);
+  return (
+    <div>
+      <a href={`${process.env.NEXT_PUBLIC_SERVER}/api/auth/oauth`}>
+        Sign in with google
+      </a>
+    </div>
+  );
+};
 
 export default Login;
