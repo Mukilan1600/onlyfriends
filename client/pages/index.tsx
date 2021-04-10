6e106c2810224d5a1cad66fa8c27a73f1a056141
mukilanik <ak-muki@hotmail.com>
2021-04-10 11:20:48 +0530

    Google logo added

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import React, { useContext, useEffect } from "react";
 import { WebSocketContext } from "../components/providers/WebSocketProvider";
+import GoogleLogoIcon from "../components/statics/icons/GoogleLogoIcon";
 import useSaveQueryParamsToken from "../components/stores/useSaveQueryParamsToken";
 
 const Login: React.FC = () => {
@@ -11,7 +12,8 @@ const Login: React.FC = () => {
     if (socketStatus === "connected") router.push("/home");
   }, [socketStatus]);
   return (
-    <div>
+    <div style={{backgroundColor: 'gray'}}>
+      <GoogleLogoIcon />
       <a href={`${process.env.NEXT_PUBLIC_SERVER}/api/auth/oauth`}>
         Sign in with google
       </a>
