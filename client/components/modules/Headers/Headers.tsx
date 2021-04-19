c31e782941a10524bf6b2d456267b0d2c15e8f72
mukilanik <ak-muki@hotmail.com>
2021-04-19 22:14:17 +0530

    Headers

---
+++ client/components/modules/Headers/Headers.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Headers() {
+    return (
+        <Head>
+            <title>Onlyfriends</title>
+        </Head>
+    )
+}
