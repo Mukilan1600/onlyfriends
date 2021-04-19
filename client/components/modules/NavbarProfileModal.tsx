a633c4c5a098372c9d25f9fe1c02bea2708af6ba
mukilanik <ak-muki@hotmail.com>
2021-04-19 21:08:45 +0530

    Change username

--- client/components/modules/NavbarProfileModal.tsx
+++ client/components/modules/NavbarProfileModal.tsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { WebSocketContext } from "../providers/WebSocketProvider";
 import EditIcon from "../statics/icons/EditIcon";
 import useProfile from "../stores/useProfile";
 import styles from "./NavbarProfileModal.module.css";
 
 export default function NavbarProfileModal() {
+  const { socket } = useContext(WebSocketContext);
   const { user } = useProfile();
+  const [isEditing, setIsEditing] = useState(false);
+  const usernameRef = useRef<HTMLParagraphElement>();
+
+  useEffect(() => {
+    usernameRef.current.innerText = user.username;
+  }, [usernameRef.current, user.username]);
+
+  const toggleEditing = () => {
+    setIsEditing((isEditing) => !isEditing);
+  };
+
+  const onUsernameSave = (event: React.FocusEvent<HTMLParagraphElement>) => {
+    if (
+      event.target.innerText.length > 0 &&
+      user.username !== event.target.innerText
+    )
+      socket.emit("change_username", { username: event.target.innerText });
+    usernameRef.current.innerText = user.username;
+    toggleEditing();
+  };
+
   return (
     <div className={styles.profileModalDiv}>
       <img src={user.avatarUrl} alt="User" id={styles.profileModalImg} />
       <div id={styles.profileModalUsernameDiv}>
-        <p id={styles.profileModalUsername}>{user.username}</p>
-        <EditIcon />
+        <p
+          id={styles.profileModalUsername}
+          contentEditable={isEditing}
+          spellCheck="false"
+          onBlur={onUsernameSave}
+          ref={usernameRef}
+        ></p>
+        <div onClick={toggleEditing} className={styles.usernameEditIcon}>
+          <EditIcon />
+        </div>
       </div>
       <div className={styles.divider} />
     </div>
