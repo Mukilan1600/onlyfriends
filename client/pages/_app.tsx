b40064cff29a0667911ad34cdcf6d0a0780f593b
mukilanik <ak-muki@hotmail.com>
2021-04-23 11:08:47 +0530

    Navigation loader

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,10 @@
 import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import 'nprogress/nprogress.css'
 import React from "react";
+import NProgress from 'nprogress'
 import { AppProps } from "next/app";
+import { Router } from 'next/router'
 import WebSocketProvider from "../components/providers/WebSocketProvider";
 import { PageComponenet } from "../types";
 import { ToastContainer } from "react-toastify";
@@ -10,6 +13,11 @@ import en from "javascript-time-ago/locale/en";
 import Headers from "../components/modules/Headers/Headers";
 
 TimeAgo.locale(en);
+
+Router.events.on("routeChangeStart", () => NProgress.start())
+Router.events.on("routeChangeComplete", () => NProgress.done())
+Router.events.on("routeChangeError", () => NProgress.done())
+
 
 const isServer = typeof window === "undefined";
 
