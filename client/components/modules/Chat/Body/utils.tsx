2e3d3cb424957ef9613f571170b4ffd34b688c6f
Mukilan IK <ak-muki@hotmail.com>
2021-05-24 21:18:03 +0530

    Fix embedded media

--- client/components/modules/Chat/Body/utils.tsx
+++ client/components/modules/Chat/Body/utils.tsx
@@ -61,9 +61,9 @@ export const formatMessage = (messageFragment: IMessageFragment, index: number, 
 
 export const formatFileMessage = (message: IMessage, prev: boolean = true, sentByMe: boolean = true) => {
   if (message.fileType === "video" && !prev) {
-    return <video src={message.fileUrl} controls style={{ width: "100%", height: "100%" }} />;
+    return <video src={message.fileUrl} controls style={{ width: "100%", height: "100%", marginBottom: '20px' }} />;
   } else if (message.fileType === "image" && !prev) {
-    return <img src={message.fileUrl} alt="image message" style={{ width: "100%", height: "100%" }} />;
+    return <img src={message.fileUrl} alt="image message" style={{ width: "100%", height: "100%", marginBottom: '15px' }} />;
   } else {
     return (
       <div
