83df8458f2d5c7d82ebc78a448a308c58ca304ec
Mukilan IK <ak-muki@hotmail.com>
2021-05-25 18:03:43 +0530

    Bug fixes

--- client/components/modules/Chat/Header/StatusIndicator/StatusIndicator.tsx
+++ client/components/modules/Chat/Header/StatusIndicator/StatusIndicator.tsx
@@ -3,11 +3,9 @@ import useChat from "../../../../stores/useChat";
 
 const StatusIndicator: React.FC = () => {
   const { chat } = useChat();
-  return (
-    <p style={{ fontSize: "12px", fontWeight: "normal", lineHeight: "16px",height: "16px" }}>
-      {chat.participants[0].user.isTyping ? "Typing..." : " "}
-    </p>
-  );
+  return chat.participants[0].user.isTyping ? (
+    <p style={{ fontSize: "12px", fontWeight: "normal", lineHeight: "16px", height: "16px" }}>Typing...</p>
+  ) : null;
 };
 
 export default StatusIndicator;
